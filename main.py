a836aab1802e5a03c2d3fb141ed782e76274f190
Marko <marko.parfeniuk.pm.2020@lpnu.ua>
2023-12-02 16:10:37 +0200

    set up bot commands

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import os
+import telebot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Read API Token from environment variables
+BOT_TOKEN: str = os.environ.get('BOT_TOKEN')
+if (not BOT_TOKEN):
+    raise Exception("Invalid BOT_TOKEN")
+if (len(BOT_TOKEN) < 10):
+    raise Exception("Invalid BOT_TOKEN")
+if (":" not in BOT_TOKEN):
+    raise Exception("Invalid BOT_TOKEN")
+# Generate bot object
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# Handle AI command
+@bot.message_handler(content_types=["text"], commands=['ai'])
+def HandleAiMessage(inputMessage: telebot.types.Message):
+    bot.reply_to(inputMessage, "* ai responds *")
+
+# Welcome new users
+@bot.message_handler(content_types=["text"], commands=['start', 'hello'])
+def send_welcome(inputMessage: telebot.types.Message):
+    bot.reply_to(inputMessage, "*Hello, " + inputMessage.from_user.first_name + "!*\nUse /help to get to know the available commands.\nHow can i assist you today?", parse_mode='Markdown')
+
+# Give project information
+@bot.message_handler(content_types=["text"], commands=['info'])
+def send_welcome(inputMessage: telebot.types.Message):
+    bot.reply_to(inputMessage, "This project is hosted on a GitHub repository. Here's the link: https://github.com/markoparfeniuk/Telegram-GPT")
+
+# Provide user with a list of commands
+@bot.message_handler(content_types=["text"], commands=['help'])
+def send_welcome(inputMessage: telebot.types.Message):
+    bot.reply_to(inputMessage, "*Let's chat!*\nTo continue, use any of the commands from the list:\n\n/start | /hello - start communicating with the bot\n/ai - for communication with AI\n/info - get more information on a project", parse_mode='Markdown')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    bot.infinity_polling()
