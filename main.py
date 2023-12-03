228726c7abdcb3ac00dc777d6d85aa9aca68d3e9
Marko <marko.parfeniuk.pm.2020@lpnu.ua>
2023-12-03 15:27:45 +0200

    update ai handler (generate temporary reply)

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import asyncio
 import os
 import telebot
 import threading
 import g4f
+import re
 
 # Read API Token from environment variables
 BOT_TOKEN: str = os.environ.get('BOT_TOKEN')
@@ -19,8 +21,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def HandleAiMessage(inputMessage: telebot.types.Message):
     # Check that the massage contains some text
     if (len(inputMessage.text) <= 5):
-        bot.reply_to(inputMessage,
-                     "Hi " + inputMessage.from_user.first_name + ",\nplease give me some data to process, syntax is: `/[command] [text to interact with]`")
+        bot.reply_to(inputMessage, "Hi " + inputMessage.from_user.first_name + ",\nplease give me some data to process, syntax is: `/[command] [text to interact with]`")
         return
     # Create async thread to handle replies
     thread = threading.Thread(target=ReplyAi, args=(inputMessage, "gpt-3.5-turbo"))
@@ -28,9 +29,21 @@ def HandleAiMessage(inputMessage: telebot.types.Message):
 
 # Create async reply
 def ReplyAi(inputMessage: telebot.types.Message, botType):
-    gptResponse: str = g4f.ChatCompletion.create(model=botType, messages=[{"role": "user", "content": inputMessage.text}])
-    bot.reply_to(inputMessage, gptResponse)
-    return
+    # Generate temporary reply
+    newReply = bot.reply_to(inputMessage, "Please wait...")
+    # Process the input query
+    inputQuery = re.sub(r"\/(\w+)", "", inputMessage.text).strip()
+    if (not inputQuery):
+        inputQuery = "Hello, who are you?"
+    # Create the GPT4FREE instance
+    gptResponse: str = g4f.ChatCompletion.create(model=botType, messages=[{"role": "user", "content": inputQuery}])
+    # Cleanup response from GPT if needed
+    gptResponse = re.sub(r"(\[\^\d\^\])", "", gptResponse)
+    # Handle some exceptions
+    if (not gptResponse):
+        gptResponse = "An empty response was returned..."
+    # Process the input text
+    bot.edit_message_text(gptResponse, inputMessage.chat.id, newReply.id)
 
 # Welcome new users
 @bot.message_handler(content_types=["text"], commands=['start', 'hello'])
